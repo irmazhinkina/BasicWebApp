708a37ac6cbafbd0daa4062a0ed9a1beed2cc4b9
Irma.Zhinkina <irma.zhinkina@jetbrains.com>
2023-08-09 16:58:06 +0200

    test

---
+++ src/test/java/de/tum/in/ase/eist/GenerateTest.java
@@ -0,0 +1,13 @@
+package de.tum.in.ase.eist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenerateTest {
+
+    @Test
+    void main() {
+
+    }
+}
